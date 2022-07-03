a17abe79496ac13984f5e324e51f1cd068c9f29a
Jiwoo <byjiwoolee@gmail.com>
2022-07-03 16:37:32 +0900

    chagne home

--- vue_shoes/src/views/Home.vue
+++ vue_shoes/src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-medium is-link mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Help me sell my roommate's shoes.</p>
+        <p class="subtitle">Shoes don't pay rent.</p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered mb-6">Latest products</h2>
+      </div>
+
+      <div 
+        class="column is-3" 
+        v-for="product in latestProducts" 
+        v-bind:key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img v-bind:src="product.get_thumbnail_url">
+          </figure>
+
+          <h3 class="is-size-5">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-2">View details</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
   components: {
-    HelloWorld
+  },
+  mounted() {
+    this.getLatestProducts()
+  },
+  methods: {
+    getLatestProducts() {
+      axios.get('api/v1/latest-products')
+          .then(response => {
+            this.latestProducts = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    }
   }
 }
 </script>
+
+<style scoped>
+  .image {
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+</style>
