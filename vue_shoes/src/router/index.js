093a0e5632024c85eb07b7a2a884ac0959230cd7
Jiwoo <byjiwoolee@gmail.com>
2022-07-06 22:46:32 +0900

    make sign up possible

--- vue_shoes/src/router/index.js
+++ vue_shoes/src/router/index.js
@@ -4,6 +4,7 @@ import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
   },
   {
     path: '/search',
