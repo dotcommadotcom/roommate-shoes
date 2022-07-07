7061b8f27ed0c39a3c0aa29e0361935088d96b3f
Jiwoo <byjiwoolee@gmail.com>
2022-07-07 16:41:28 +0900

    create order app

---
+++ django_shoes/order/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import Http404
+
+from rest_framework import status, authentication, permissions
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.views import APIView
+from rest_framework.response import Response
